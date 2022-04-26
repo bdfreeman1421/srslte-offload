d3831ec074a0d7a00ad47d1d8351568b5918a9ea
Brian Freeman <bfreeman@optonline.net>
2022-04-26 21:04:42 +0000

    update
    Signed-off-by: Brian Freeman <bfreeman@optonline.net>

---
+++ startit.sh
@@ -0,0 +1,3 @@
+# sudo ./srsepc/src/srsepc
+# ./srsenb/src/srsenb --rf.device_name=zmq --rf.device_args="fail_on_disconnect=true,tx_port=tcp://*:2000,rx_port=tcp://localhost:2001,id=enb,base_srate=23.04e6"
+# sudo ./srsue/src/srsue --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --gw.netns=ue1
